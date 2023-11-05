d26ef508b1843dfc81e0912e3f174f19976fdf8b
Hammad Maqbool <hammad.mqbl@gmail.com>
2023-11-05 11:37:04 +0500

    W1D1

--- Week 1/Day 1/topic-covered-today.js
+++ Week 1/Day 1/topic-covered-today.js
@@ -1,19 +1,32 @@
-console.log((10+2)*5)
-console.log("This is the text to show\nThis is the second line")
-console.log("Con"+"cat"+"e"+"nate")
+//console.log((10+2)*5)
+//console.log("This is the text to show\nThis is the second line")
+//console.log("Con"+"cat"+"e"+"nate")
 
 //Backticked-quote
-console.log(`The half of 100 is ${100/2}`)
+//console.log(`The half of 100 is ${100/2}`)
 
-console.log(typeof "Its me Hammad")
+//console.log(typeof "Its me Hammad")
 
-console.log(14>10)
+//console.log(14>10)
 
 //Compares on the base of Unicode
-console.log("A" < "Z")
+//console.log("A" < "Z")
 
 //======Automatic Type COnversion======//
-console.log(5+5)
-console.log("Hammad"+"Maqbool")
-console.log(4+'5')
-console.log(4-"5")
+//console.log(5+5)
+//console.log("Hammad"+"Maqbool")
+//console.log(4+'5')
+//console.log(4-"5")
+
+//=================================
+//Concept of Variables or Bindings
+//=================================
+
+//let myVar = 5+10
+//console.log(myVar)
+
+//let myValueOne = 10;
+//let myValueTwo = 15;
+//console.log("Result is "+(myValueOne+myValueTwo))
+//let finalResult = myValueOne+myValueTwo
+//console.log(finalResult)
